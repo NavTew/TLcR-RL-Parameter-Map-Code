ebea164d17b2ff021bdb8a087f22ec1fa85986a0
Navya Tewari <tewarinavya@gmail.com>
2025-02-26 23:19:56 +0530

    probably correct complete code

--- TLcR-RL/Demo_TLcR_RL.m
+++ TLcR-RL/Demo_TLcR_RL.m
@@ -7,8 +7,8 @@ clc;close all;clear all;
 addpath('.\utilities');
 
 
-nTraining   = 1;        % number of training sample
-nTesting    = 2;         % number of ptest sample
+nTraining   = 360;        % number of training sample
+nTesting    = 1;         % number of ptest sample
 upscale     = 4;          % upscaling factor 
 patch_size  = 12;         % image patch size
 overlap     = 4;          % the overlap between neighborhood patches
@@ -43,11 +43,11 @@ zero_matrix = zeros(U, V);
 fprintf('\nStarting to make K-Map')
 
 
-for TestImgIndex = 1:nTesting
-    fprintf('\nProcessing  %d_test.jpg\n', TestImgIndex);
+for TrainImgIndex = 1:1
+    fprintf('\nProcessing  %d_test.jpg\n', TrainImgIndex);
 
     % read ground truth of one test face 
-    strh    = strcat('.\testFaces\',num2str(TestImgIndex),'_test.jpg');
+    strh    = strcat('.\testFaces\',num2str(TrainImgIndex),'_test.jpg');
     im_h    = double(imread(strh));
 
     % generate the input LR face by smooth and down-sampleing
@@ -62,14 +62,14 @@ for TestImgIndex = 1:nTesting
     [im_SR] = TLcR_RL(im_b,YH,YL,upscale,patch_size,overlap,stepsize,window,tau,K,c);     
     % add the high frequency face to result
     [im_SR] = im_SR+im_b;
-    cputime(TestImgIndex) = toc;
-    imwrite(uint8(im_SR),strcat('./results/',num2str(TestImgIndex),'_',num2str(tau),'_TLcR.bmp'),'bmp');  
+    cputime(TrainImgIndex) = toc;
+    imwrite(uint8(im_SR),strcat('./results/',num2str(TrainImgIndex),'_',num2str(tau),'_TLcR.bmp'),'bmp');  
 
     % compute PSNR and SSIM for Bicubi,c and TLcR method
-    bicubic_psnr(TestImgIndex) = psnr(im_b,im_h);
-    bicubic_ssim(TestImgIndex) = ssim(im_b,im_h);
-    TLcR_psnr(TestImgIndex) = psnr(im_SR,im_h);
-    TLcR_ssim(TestImgIndex) = ssim(im_SR,im_h);  
+    bicubic_psnr(TrainImgIndex) = psnr(im_b,im_h);
+    bicubic_ssim(TrainImgIndex) = ssim(im_b,im_h);
+    TLcR_psnr(TrainImgIndex) = psnr(im_SR,im_h);
+    TLcR_ssim(TrainImgIndex) = ssim(im_SR,im_h);  
 
 
     % % updata the result by reproducing learning
@@ -80,10 +80,49 @@ for TestImgIndex = 1:nTesting
     %     [im_SR] = TLcR_RL(im_b,cat(3,YH,im_SR-im_lSR),cat(3,YL,im_lSR),upscale,patch_size,overlap,stepsize,window,tau,K,c);
     %     [im_SR] = im_SR+im_b;
     %     % compute PSNR and SSIM for Bicubic and TLcR-RL method
-    %     TLcRRL_psnr(ls,TestImgIndex) = psnr(im_SR,im_h);
-    %     TLcRRL_ssim(ls,TestImgIndex) = ssim(im_SR,im_h);          
+    %     TLcRRL_psnr(ls,TrainImgIndex) = psnr(im_SR,im_h);
+    %     TLcRRL_ssim(ls,TrainImgIndex) = ssim(im_SR,im_h);          
     % end 
-    % imwrite(uint8(im_SR),strcat('./results/',num2str(TestImgIndex),'_',num2str(tau),'_TLcRRL.bmp'),'bmp');  
+    % imwrite(uint8(im_SR),strcat('./results/',num2str(TrainImgIndex),'_',num2str(tau),'_TLcRRL.bmp'),'bmp');  
+    
+    % display the objective results (PSNR and SSIM)
+    fprintf('PSNR for Bicubic:  %f dB\n', bicubic_psnr(TrainImgIndex));
+    fprintf('PSNR for TLcR:     %f dB\n', TLcR_psnr(TrainImgIndex));
+%    fprintf('PSNR for TLcR-RL:  %f dB\n', TLcRRL_psnr(layer,TrainImgIndex));
+    fprintf('SSIM for Bicubic:  %f dB\n', bicubic_ssim(TrainImgIndex));
+    fprintf('SSIM for TLcR:     %f dB\n', TLcR_ssim(TrainImgIndex));
+%    fprintf('SSIM for TLcR-RL:  %f dB\n', TLcRRL_ssim(layer,TrainImgIndex));
+    disp(zero_matrix);
+end 
+
+zero_matrix = round(zero_matrix / 1);
+disp(zero_matrix)
+
+nTesting = 11;
+for TestImgIndex = 9:nTesting
+    fprintf('\nProcessing  %d_test.jpg\n', TestImgIndex); 
+    strh    = strcat('.\testFaces\',num2str(TestImgIndex),'_test.jpg');
+    im_h    = double(imread(strh));
+    psf         = fspecial('average', [4 4]); 
+    im_s    = imfilter(im_h,psf);
+    im_l    = imresize(im_s,1/upscale,'bicubic');
+    im_b = imresize(im_l,upscale,'bicubic');
+    
+    tic;
+    % hallucinate the high frequency face via TLcR
+    [im_SR] = my_TLcR_RL(im_b,YH,YL,upscale,patch_size,overlap,stepsize,window,tau,K,c);     
+    % add the high frequency face to result
+    [im_SR] = im_SR+im_b;
+    cputime(TestImgIndex) = toc;
+    imwrite(uint8(im_SR),strcat('./results/',num2str(TestImgIndex),'_',num2str(tau),'_TLcR.bmp'),'bmp');  
+
+    % compute PSNR and SSIM for Bicubi,c and TLcR method
+    bicubic_psnr(TestImgIndex) = psnr(im_b,im_h);
+    bicubic_ssim(TestImgIndex) = ssim(im_b,im_h);
+    TLcR_psnr(TestImgIndex) = psnr(im_SR,im_h);
+    TLcR_ssim(TestImgIndex) = ssim(im_SR,im_h);  
+
+
     
     % display the objective results (PSNR and SSIM)
     fprintf('PSNR for Bicubic:  %f dB\n', bicubic_psnr(TestImgIndex));
@@ -93,55 +132,16 @@ for TestImgIndex = 1:nTesting
     fprintf('SSIM for TLcR:     %f dB\n', TLcR_ssim(TestImgIndex));
 %    fprintf('SSIM for TLcR-RL:  %f dB\n', TLcRRL_ssim(layer,TestImgIndex));
     disp(zero_matrix);
-end 
 
-zero_matrix = round(zero_matrix / 2);
-disp(zero_matrix)
-
-% nTesting = 11;
-% for TestImgIndex = 9:nTesting
-%     fprintf('\nProcessing  %d_test.jpg\n', TestImgIndex); 
-%     strh    = strcat('.\testFaces\',num2str(TestImgIndex),'_test.jpg');
-%     im_h    = double(imread(strh));
-%     psf         = fspecial('average', [4 4]); 
-%     im_s    = imfilter(im_h,psf);
-%     im_l    = imresize(im_s,1/upscale,'bicubic');
-%     im_b = imresize(im_l,upscale,'bicubic');
-    
-%     tic;
-%     % hallucinate the high frequency face via TLcR
-%     [im_SR] = my_TLcR_RL(im_b,YH,YL,upscale,patch_size,overlap,stepsize,window,tau,K,c);     
-%     % add the high frequency face to result
-%     [im_SR] = im_SR+im_b;
-%     cputime(TestImgIndex) = toc;
-%     imwrite(uint8(im_SR),strcat('./results/',num2str(TestImgIndex),'_',num2str(tau),'_TLcR.bmp'),'bmp');  
-
-%     % compute PSNR and SSIM for Bicubi,c and TLcR method
-%     bicubic_psnr(TestImgIndex) = psnr(im_b,im_h);
-%     bicubic_ssim(TestImgIndex) = ssim(im_b,im_h);
-%     TLcR_psnr(TestImgIndex) = psnr(im_SR,im_h);
-%     TLcR_ssim(TestImgIndex) = ssim(im_SR,im_h);  
+end
 
 
-    
-%     % display the objective results (PSNR and SSIM)
-%     fprintf('PSNR for Bicubic:  %f dB\n', bicubic_psnr(TestImgIndex));
-%     fprintf('PSNR for TLcR:     %f dB\n', TLcR_psnr(TestImgIndex));
-% %    fprintf('PSNR for TLcR-RL:  %f dB\n', TLcRRL_psnr(layer,TestImgIndex));
-%     fprintf('SSIM for Bicubic:  %f dB\n', bicubic_ssim(TestImgIndex));
-%     fprintf('SSIM for TLcR:     %f dB\n', TLcR_ssim(TestImgIndex));
-% %    fprintf('SSIM for TLcR-RL:  %f dB\n', TLcRRL_ssim(layer,TestImgIndex));
-%     disp(zero_matrix);
-
-% end
-
-
-% % fprintf('===============================================\n');
-% % fprintf('Average PSNR for Bicubic:  %f dB\n', sum(bicubic_psnr)/nTesting);
-% % fprintf('Average PSNR for TLcR:     %f dB\n', sum(TLcR_psnr)/nTesting);
-% % fprintf('Average PSNR for TLcRRL:   %f dB\n', sum(TLcRRL_psnr(layer,:))/nTesting);
-% % fprintf('Average SSIM for Bicubic:  %f dB\n', sum(bicubic_ssim)/nTesting);
-% % fprintf('Average SSIM for TLcR:     %f dB\n', sum(TLcR_ssim)/nTesting);
-% % fprintf('Average SSIM for TLcR:     %f dB\n', sum(TLcRRL_ssim(layer,:))/nTesting);
-% % fprintf('===============================================\n');
+fprintf('===============================================\n');
+fprintf('Average PSNR for Bicubic:  %f dB\n', sum(bicubic_psnr)/nTesting);
+fprintf('Average PSNR for TLcR:     %f dB\n', sum(TLcR_psnr)/nTesting);
+fprintf('Average PSNR for TLcRRL:   %f dB\n', sum(TLcRRL_psnr(layer,:))/nTesting);
+fprintf('Average SSIM for Bicubic:  %f dB\n', sum(bicubic_ssim)/nTesting);
+fprintf('Average SSIM for TLcR:     %f dB\n', sum(TLcR_ssim)/nTesting);
+fprintf('Average SSIM for TLcR:     %f dB\n', sum(TLcRRL_ssim(layer,:))/nTesting);
+fprintf('===============================================\n');
 
--- TLcR-RL/utilities/TLcR_RL.m
+++ TLcR-RL/utilities/TLcR_RL.m
@@ -1,111 +1,3 @@
-
-% function [im_SR] = TLcR_RL(im_l,YH,YL,upscale,patch_size,overlap,stepsize,window,tau,K,c)
-%     global zero_matrix
-%     [imrow, imcol, nTraining] = size(YH);
-%     Img_SUM      = zeros(imrow,imcol);
-%     overlap_FLAG = zeros(imrow,imcol);
-    
-%     U = ceil((imrow-overlap)/(patch_size-overlap));  
-%     V = ceil((imcol-overlap)/(patch_size-overlap)); 
-    
-%     for i = 1:U
-%         fprintf('.');
-%          for j = 1:V  
-%             fprintf('.')
-%             % obtain the current patch position
-%             BlockSize  =  GetCurrentBlockSize(imrow,imcol,patch_size,overlap,i,j);    
-%             if size(YL,1) == size(YH,1)
-%                 BlockSizeS =  GetCurrentBlockSize(imrow,imcol,patch_size,overlap,i,j);  
-%             else
-%                 BlockSizeS =  GetCurrentBlockSize(size(YL,1),size(YL,2),patch_size/upscale,overlap/upscale,i,j);  
-%             end
-            
-%             % obtain the current patch feature
-%             im_l_patch =  im_l(BlockSizeS(1):BlockSizeS(2),BlockSizeS(3):BlockSizeS(4));           % extract the patch at position（i,j）of the input LR face     
-%             im_l_patch =  im_l_patch(:);   
-%             im_l_patch = im_l_patch-mean(im_l_patch);
-%             im_l_patch = [im_l_patch;0;0]; %(0,0) is the spatial information of current LR patch
-            
-%             % obtain the LR and HR training patches
-%             padpixel = (window-patch_size)/stepsize;
-%             XF = Reshape3D_20Connection(YH,BlockSize,stepsize,padpixel);
-%             X  = Reshape3D_20Connection_Spatial(YL,BlockSizeS,stepsize,padpixel,c);        
-           
-%             % obtain the LR training patch feature by subtracting its mean
-%             X(1:end-2,:) = X(1:end-2,:)-repmat(mean(X(1:end-2,:)),size(X(1:end-2,:),1),1);
-         
-%             % calculate the distances between current patch and the LR training patches
-%             nframe =  size(im_l_patch',1);
-%             nbase  =  size(X',1);
-%             XX     =  sum(im_l_patch'.*im_l_patch', 2);        
-%             SX     =  sum(X'.*X', 2);
-%             D      =  repmat(XX, 1, nbase)-2*im_l_patch'*X+repmat(SX', nframe, 1);        
-    
-              
-
-
-%             K_list = 1:7;
-
-%             best_K = K_list(1);  
-%             min_error = Inf;   % Initialize with a large value
-
-%             for k_idx = 1:length(K_list)
-%                 K = K_list(k_idx);  % Select current K
-                
-%                 % Find the K nearest neighbors
-%                 [val, index] = sort(D);        
-%                 Xk  = X(:, index(1:K));        
-%                 XFk = XF(:, index(1:K));      
-%                 Dk  = D(index(1:K));
-
-%                 % Compute the weight vector
-%                 z   = Xk' - repmat(im_l_patch', K, 1);         
-%                 C   = z * z';                                                  
-%                 C   = C + tau * diag(Dk) + eye(K,K) * (1e-6) * trace(C);   
-%                 w   = C \ ones(K,1);  
-%                 w   = w / sum(w);    
-
-%                 % Reconstruct LR patch using weighted combination of neighbors
-%                 im_l_patch_recon = Xk * w;  
-
-%                 % Compute reconstruction error (L2 norm)
-%                 error = norm(im_l_patch - im_l_patch_recon, 2);  
-
-%                 % Choose the best K with minimum reconstruction error
-%                 if error < min_error
-%                     min_error = error;
-%                     best_K = K;
-%                     best_Xk = Xk;
-%                     best_XFk = XFk;
-%                     best_w = w;
-%                 end
-%             end
-%             fprintf('Best K selected: %d\n', best_K);
-%             old_value = zero_matrix(i, j)
-%             zero_matrix(i, j) = old_value + best_K;
-
-
-%            % Obtain the HR patch with the best weight vector w
-%             Img  = best_XFk * best_w; 
-%             Img  = reshape(Img, patch_size, patch_size);
-
-%             % obtain the HR patch with the same weight vector w
-%             % Img  =  XFk*w; 
-            
-%             % integrate all the LR patch        
-%             Img  =  reshape(Img,patch_size,patch_size);
-%             Img_SUM(BlockSize(1):BlockSize(2),BlockSize(3):BlockSize(4))      = Img_SUM(BlockSize(1):BlockSize(2),BlockSize(3):BlockSize(4))+Img;
-%             overlap_FLAG(BlockSize(1):BlockSize(2),BlockSize(3):BlockSize(4)) = overlap_FLAG(BlockSize(1):BlockSize(2),BlockSize(3):BlockSize(4))+1;
-%         end
-%     end
-%     %  averaging pixel values in the overlapping regions
-%     im_SR = Img_SUM./overlap_FLAG;
-%     fprintf('\n');
-%     % save('XXFXX.mat','xXF','xX');
-
-
-
-
 function [im_SR] = TLcR_RL(im_l,YH,YL,upscale,patch_size,overlap,stepsize,window,tau,K,c)
     global zero_matrix
     [imrow, imcol, nTraining] = size(YH);
@@ -116,7 +8,6 @@ function [im_SR] = TLcR_RL(im_l,YH,YL,upscale,patch_size,overlap,stepsize,window
     V = ceil((imcol-overlap)/(patch_size-overlap)); 
     
     for i = 1:U
-        fprintf('.');
          for j = 1:V  
             fprintf('.');
             % obtain the current patch position
@@ -144,37 +35,37 @@ function [im_SR] = TLcR_RL(im_l,YH,YL,upscale,patch_size,overlap,stepsize,window
             SX     =  sum(X'.*X', 2);
             D      =  repmat(XX, 1, nbase) - 2*im_l_patch'*X + repmat(SX', nframe, 1);        
             
-            fprintf("DE starting for %d %d\n", U, V);
+            fprintf("DE starting for %d %d\n", i, j);
 
             % Differential Evolution (DE) Implementation
             pop_size = 10;
             max_gen = 50;
             F = 0.8;
             CR = 0.9;
-            K_bounds = [1, 9];
+            K_bounds = [1, 3240];
             
             pop = randi(K_bounds, [pop_size, 1]);
             fitness = arrayfun(@(k) computeError(k, D, X, XF, im_l_patch, tau), pop);
             
             for gen = 1:max_gen
-                fprintf("\nGeneration Number %d\n", gen)
+                
                 for p = 1:pop_size
                     r = randperm(pop_size, 3);
                     while any(r == p)
                         r = randperm(pop_size, 3);
                     end
-                    fprintf("!!!_")
+                    
                     mutant = pop(r(1)) + F * (pop(r(2)) - pop(r(3)));
                     mutant = max(min(mutant, K_bounds(2)), K_bounds(1));
-                    fprintf("!!!_")
+                    
                     if rand < CR
                         trial = round(mutant);
                     else
                         trial = pop(p);
                     end
-                    fprintf("!!!_")
+                    
                     trial_fitness = computeError(trial, D, X, XF, im_l_patch, tau);
-                    fprintf("trial_fitness %d", trial_fitness)
+                    
                     if trial_fitness < fitness(p)
                         pop(p) = trial;
                         fitness(p) = trial_fitness;
@@ -185,7 +76,7 @@ function [im_SR] = TLcR_RL(im_l,YH,YL,upscale,patch_size,overlap,stepsize,window
             [~, best_idx] = min(fitness);
             best_K = pop(best_idx);
 
-            fprintf('Best K selected: %d\n', best_K);
+            
             old_value = zero_matrix(i, j);
             zero_matrix(i, j) = old_value + best_K;
             
